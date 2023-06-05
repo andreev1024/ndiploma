d69cc86a7320e8afbf99723079cc6111dbd98c26
Nastya <serhiyenkanastya@gmail.com>
2023-06-05 20:24:47 +0300

    change DB

--- fakedb.sql
+++ fakedb.sql
@@ -1,5 +1,5 @@
 create table Client{
-    id_client SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     client_surename varchar(100) not null,
     client_name varchar(100) not null,
     client_secondname varchar(100) not null,
@@ -7,57 +7,57 @@ create table Client{
 }
 
 create table Application{
-    id_application SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     id_client int not null,
     id_teacher int not null,
     application_date date not null,
     application_time time not null,
-    FOREIGN KEY (id_client) REFERENCES Application (id_client),
-    FOREIGN KEY (id_teacher) REFERENCES Teacher (id_teacher)
+    FOREIGN KEY (id_client) REFERENCES Client (id),
+    FOREIGN KEY (id_teacher) REFERENCES Teacher (id)
 }
 
 create table Teacher{
-    id_teacher SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY
     teacher_surename varchar(100) not null,
     teacher_name varchar(100) not null,
     teacher_post varchar(100) not null,
     id_schedule int not null,
-    FOREIGN KEY (id_schedule) REFERENCES Schedule (id_schedule)
+    FOREIGN KEY (id_schedule) REFERENCES Schedule (id)
 }
 
 create table Schedule{
-    id_schedule SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     start_time time not null,
     end_time time not null,
     workday varchar(100) not null
 }
 
 create table GroupEvent{
-    id_group_event SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     id_theme int not null,
     id_teacher int not null,
     class varchar(10) not null,
-    FOREIGN KEY (id_theme) REFERENCES Theme (id_theme),
-    FOREIGN KEY (id_teacher) REFERENCES Teacher (id_teacher)
+    FOREIGN KEY (id_theme) REFERENCES Theme (id),
+    FOREIGN KEY (id_teacher) REFERENCES Teacher (id)
 }
 
 create table Meeting{
-    id_meeting SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     id_theme int not null,
     id_application int not null,
     meeting_date date not null,
     meeting_time time not null,
-    FOREIGN KEY (id_theme) REFERENCES Theme (id_theme),
-    FOREIGN KEY (id_application) REFERENCES Application (id_application)
+    FOREIGN KEY (id_theme) REFERENCES Theme (id),
+    FOREIGN KEY (id_application) REFERENCES Application (id)
 }
 
 create table Theme{
-    id_theme SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     Theme varchar(100) not null,
     id_category int not null,
-    FOREIGN KEY (id_category) REFERENCES Category (id_category)
+    FOREIGN KEY (id_category) REFERENCES Category (id)
 }
 create table Category{
-    id_category SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     category varchar(100) not null
 }
